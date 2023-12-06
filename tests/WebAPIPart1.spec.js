4ea119984cde748e9152f5fbf067c22141ea8a8c
Thai Ngo Anh <thaina@vmogroup.com>
2023-12-06 14:53:53 +0700

    a

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -1,32 +1,13 @@
 const { test, expect, request } = require('@playwright/test');
+const { ApiUtils } = require('./utils/ApiUtils');
 const loginPayload = { userEmail: "thai@gmail.com", userPassword: "4Youonly4" };
-const orderPayload = { orders: [{ country: "Vietnam", productOrderedId: "6262e95ae26b7e1a10e89bf0" }] };
-let token;
-let orderID;
+const orderPayload = { orders: [{ country: "Vietnam", productOrderedId: "6262e990e26b7e1a10e89bfa" }] };
+let response;
 
 test.beforeAll(async () => {
-    //login API
     const apiContext = await request.newContext();
-    const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login", {
-        data: loginPayload
-    });
-    expect(loginResponse.ok()).toBeTruthy();
-    const loginResponseJson = await loginResponse.json();
-    token = loginResponseJson.token;
-    console.log(token);
-    //create order API
-    const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
-        {
-            data: orderPayload,
-            headers:
-            {
-                'Authorization': token,
-                'Content-Type': 'application/json'
-            }
-        });
-    const orderResponseJson = await orderResponse.json();
-    console.log(orderResponseJson);
-    orderID = await orderResponseJson.orders[0];
+    const apiUtils = new ApiUtils(apiContext, loginPayload);
+    response = await apiUtils.createOrder(orderPayload);
 });
 
 
@@ -34,7 +15,7 @@ test('API login bypass', async ({ page }) => {
     await page.addInitScript(value => {
         window.localStorage.setItem('token', value);
 
-    }, token);
+    }, response.token);
     const productName = "adidas original";
     await page.goto("https://rahulshettyacademy.com/client/");
     await page.locator("button[routerlink='/dashboard/myorders']").click();
@@ -43,13 +24,13 @@ test('API login bypass', async ({ page }) => {
     const ordersCount = await allOrders.count();
     for (let i = 0; i < ordersCount; ++i) {
         const historyOrderId = await allOrders.locator("th").nth(i).textContent();
-        if (historyOrderId === orderID) {
+        if (historyOrderId === response.orderId) {
             await allOrders.nth(i).locator("button").first().click();
             break;
         }
     }
     const orderIdDetails = await page.locator("div .col-text").textContent();
     await page.pause();
-    expect(orderID.includes(orderIdDetails)).toBeTruthy();
+    expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
 
 })
